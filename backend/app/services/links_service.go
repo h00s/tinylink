b3b940009eaec97b09da1b4ae19bdc57d1b3f505
Krunoslav Husak <kruno@husak.me>
2024-04-12 11:53:11 +0200

    Check if URL already exists on link create

--- backend/app/services/links_service.go
+++ backend/app/services/links_service.go
@@ -10,21 +10,21 @@ type LinksService struct {
 	raptor.Service
 }
 
-func (l *LinksService) Get(id uint) (models.Link, error) {
+func (ls *LinksService) Get(id uint) (models.Link, error) {
 	var link models.Link
-	if err := l.DB.First(&link, id).Error; err != nil {
+	if err := ls.DB.First(&link, id).Error; err != nil {
 		return link, err
 	}
 	return link, nil
 }
 
-func (l *LinksService) GetByShortID(shortID string) (models.Link, error) {
-	return l.Get(internal.IDFromShortURI(shortID))
+func (ls *LinksService) GetByShortID(shortID string) (models.Link, error) {
+	return ls.Get(internal.IDFromShortURI(shortID))
 }
 
-func (l *LinksService) GetByURL(url string) (models.Link, error) {
+func (ls *LinksService) GetByURL(url string) (models.Link, error) {
 	var link models.Link
-	if err := l.DB.
+	if err := ls.DB.
 		Where("url = ?", url).
 		First(&link).Error; err != nil {
 		return link, err
@@ -32,16 +32,18 @@ func (l *LinksService) GetByURL(url string) (models.Link, error) {
 	return link, nil
 }
 
-func (l *LinksService) Create(link models.Link) (models.Link, error) {
+func (ls *LinksService) Create(link models.Link) (models.Link, error) {
 	if err := internal.IsURLValid(link.URL); err != nil {
 		return link, err
 	}
-	// find if url already exists
-	err := l.DB.
+	if l, err := ls.GetByURL(link.URL); err == nil {
+		return l, nil
+	}
+	err := ls.DB.
 		Select(models.LinkPermittedParams).
 		Create(&link).Error
 	if err != nil {
 		return link, err
 	}
-	return l.Get(link.ID)
+	return ls.Get(link.ID)
 }
