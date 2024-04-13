ae2c83070fe51ff60b90002a337a2599fbeff345
Krunoslav Husak <kruno@husak.me>
2024-04-13 09:29:48 +0200

    Initial design and routes added

---
+++ frontend/src/routes/[link]/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  export let data;
+</script>
+
+<div>
+  <h1>{data.link}</h1>
+</div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background-dark': '#011627',
+        'background-light': '#fff',
+      },
+    },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        'light': {
+          'base-content': '#000',
+          primary: '#0fa5e9',
+        },
+      },
+      {
+        'dark': {
+          'base-content': '#fff',
+        },
+      }
+    ],
+  },
 }
+
+// 011627
