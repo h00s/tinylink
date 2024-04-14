d8d8d75bb41a15e5762e930690da1a04dd1cd612
Krunoslav Husak <kruno@husak.me>
2024-04-14 08:15:38 +0200

    Fix shorturi parsing when using invalid chars

--- backend/internal/shorturi.go
+++ backend/internal/shorturi.go
@@ -16,6 +16,9 @@ func ShortURIfromID(id uint) string {
 func IDFromShortURI(uri string) uint {
 	id := uint(0)
 	for _, c := range uri {
+		if !strings.Contains(validChars, string(c)) {
+			return 0
+		}
 		id = id*uint(len(validChars)) + uint(strings.Index(validChars, string(c)))
 	}
 	return id
