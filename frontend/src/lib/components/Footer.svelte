779fa6bfe08c6348d8327966658867d134d22a0c
Krunoslav Husak <kruno@husak.me>
2024-05-04 23:59:43 +0200

    Updated footer

--- frontend/src/lib/components/Footer.svelte
+++ frontend/src/lib/components/Footer.svelte
@@ -1,3 +1,3 @@
-<footer class="text-sm">
-  <a href="https://github.com/h00s">Izrađeno s 💚</a>
+<footer class="text-xs text-light-green">
+  &copy; 2024. <a href="https://github.com/h00s">Krunoslav Husak</a> | <a href="https://github.com/h00s/tinylink">GitHub</a> | Izrađeno s 💚
 </footer>
