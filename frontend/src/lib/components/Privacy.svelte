409a5f041ce2d647a4df2cab83fe990220946efa
Krunoslav Husak <kruno@husak.me>
2024-05-04 23:12:53 +0200

    Privacy component styling

--- frontend/src/lib/components/Privacy.svelte
+++ frontend/src/lib/components/Privacy.svelte
@@ -5,7 +5,7 @@
 <FlexContainer>
   <div class="mt-4">
     <p class="text-sm text-center text-gray-500">
-      Korištenjem ove usluge pristajete na <a href="/terms" class="text-blue">uvjete korištenja</a> i <a href="/privacy" class="text-blue">pravila privatnosti</a>
+      Korištenjem ove usluge pristajete na <a href="/terms" class="text-blue hover:text-light-green">uvjete korištenja</a> i <a href="/privacy" class="text-blue hover:text-light-green">pravila privatnosti</a>
     </p>
   </div>
 </FlexContainer>
