08f051e978b38bfc7906927d1afda7c98e704a53
Krunoslav Husak <kruno@husak.me>
2024-05-04 02:02:34 +0200

    Added terms and privacy pages

---
+++ frontend/src/routes/privacy/+page.svelte
@@ -0,0 +1,51 @@
+<script>
+  import FlexContainer from '$lib/components/FlexContainer.svelte'
+  import Authorize from '$lib/components/Authorize.svelte'
+  import Privacy from '$lib/components/Privacy.svelte'
+  import Footer from '$lib/components/Footer.svelte'
+</script>
+
+<FlexContainer>
+  <h1 class="title pt-16 pb-2 font-extrabold text-center">skrati.link</h1>
+  <h2 class="pt-4 text-2xl font-extrabold text-center">
+    Pravila privatnosti
+  </h2>
+
+  <p>
+    Ova Pravila privatnosti opisuju kako mi prikupljamo, koristimo i otkrivamo osobne podatke kada koristite našu uslugu i objašnjavaju vaša prava u vezi s tim podacima. Molimo vas pažljivo pročitajte ova pravila privatnosti prije nego što koristite našu uslugu.
+  </p>
+  <p>
+    <b class="text-light-green">Prikupljanje podataka:</b> Pri korištenju naše usluge možemo prikupljati određene osobne podatke kao što su IP adrese, podaci o pregledniku, podaci o lokaciji i druge tehničke informacije koje nam pomažu poboljšati uslugu.
+  </p>
+  <p>
+    <b class="text-light-green">Upotreba podataka:</b> Osobne podatke koristimo isključivo radi pružanja i poboljšanja naše usluge. Ti podaci mogu se koristiti i za analizu trendova, administraciju web mjesta, praćenje kretanja korisnika i prikupljanje demografskih podataka.
+  </p>
+  <p>
+    <b class="text-light-green">Otkrivanje podataka:</b> Nećemo prodavati, trgovati ili na bilo koji drugi način prenositi vaše osobne podatke trećim stranama bez vašeg pristanka, osim ako to nije potrebno radi pružanja usluge ili ako to zahtijeva zakon.
+  </p>
+  <p>
+    <b class="text-light-green">Sigurnost podataka:</b> Poduzimamo razumne mjere kako bismo zaštitili vaše osobne podatke od neovlaštenog pristupa, izmjene ili uništenja. Međutim, imajte na umu da nijedna metoda prijenosa preko interneta ili metoda elektroničke pohrane nije 100% sigurna i stoga ne možemo jamčiti apsolutnu sigurnost vaših podataka.
+  </p>
+  <p>
+    <b class="text-light-green">Dječja privatnost:</b> Naša usluga nije namijenjena osobama mlađim od 13 godina. Ne prikupljamo namjerno osobne podatke od djece mlađe od 13 godina. Ako saznamo da smo prikupili osobne podatke djeteta mlađeg od 13 godina, odmah ćemo poduzeti korake kako bismo te podatke izbrisali.
+  </p>
+  <p>  
+    <b class="text-light-green">Izmjene Pravila privatnosti:</b> Zadržavamo pravo izmjene ovih Pravila privatnosti u bilo kojem trenutku. Bilo kakve promjene bit će objavljene na ovoj stranici, stoga redovito provjeravajte ovu stranicu radi ažuriranja.
+  </p>
+  <p>
+    Vaša upotreba naše usluge podrazumijeva vaše prihvaćanje ovih Pravila privatnosti. Ako imate bilo kakvih pitanja ili nedoumica, slobodno nas kontaktirajte.
+  </p>
+
+  <p>
+    Datum zadnje izmjene: 4.5.2024.
+  </p>
+  
+</FlexContainer>
+
+<Footer />
+
+<style>
+  p {
+    margin-top: 1rem;
+  }
+</style>
---
+++ frontend/src/routes/terms/+page.svelte
@@ -0,0 +1,60 @@
+<script>
+  import FlexContainer from '$lib/components/FlexContainer.svelte'
+  import Authorize from '$lib/components/Authorize.svelte'
+  import Privacy from '$lib/components/Privacy.svelte'
+  import Footer from '$lib/components/Footer.svelte'
+</script>
+
+<FlexContainer>
+  <h1 class="title pt-16 pb-2 font-extrabold text-center">skrati.link</h1>
+  <h2 class="pt-4 text-2xl font-extrabold text-center">
+    Uvjeti korištenja
+  </h2>
+
+  <p>Korištenjem našeg usluga, pristajete na sljedeće uvjete korištenja. Molimo vas pažljivo pročitajte sljedeće informacije:</p>
+
+  <p>
+    <b class="text-light-green">Prihvaćanje uvjeta:</b> Pristupom i korištenjem naše usluge, pristajete na sve uvjete i odredbe ove stranice. Ako se ne slažete s bilo kojim dijelom ovih uvjeta, nemojte koristiti našu uslugu.
+  </p>
+  
+  <p>
+    <b class="text-light-green">Odgovornost korisnika:</b> Korištenje našeg servisa je isključivo na vašu vlastitu odgovornost. Vi ste odgovorni za bilo kakvu štetu ili gubitak koji proizađe iz korištenja naših usluga.
+  </p>
+
+  <p>
+    <b class="text-light-green">Odgovornost vlasnika:</b> Vlasnik ovog servisa ne preuzima nikakvu odgovornost za sadržaj na koji se korisnici preusmjeravaju korištenjem našeg servisa. Ne garantiramo sigurnost, pouzdanost ili valjanost sadržaja na takvim stranicama.
+  </p>
+
+  <p>
+    <b class="text-light-green">Intelektualno vlasništvo:</b> Sadržaj koji pružamo, uključujući ali ne ograničavajući se na logotipe, tekst, grafike, fotografije, videozapise i ostale materijale, zaštićen je autorskim pravima i drugim zakonima o intelektualnom vlasništvu.
+  </p>  
+
+  <p>
+    <b class="text-light-green">Izmjene uvjeta:</b> Zadržavamo pravo izmjene ovih uvjeta korištenja u bilo kojem trenutku bez prethodne obavijesti. Vaša daljnja upotreba naše usluge nakon takvih promjena smatrat će se vašim prihvaćanjem izmjena.
+  </p>
+
+  <p>
+    <b class="text-light-green">Završne odredbe:</b> Ovi uvjeti korištenja predstavljaju cjelokupni dogovor između vas i nas u vezi s korištenjem naše usluge. Ako bilo koja odredba ovih uvjeta bude smatrana nevaljanom ili neizvršivom, ta odredba će se smatrati odvojivom od preostalih odredbi, koje će ostati u punoj snazi i učinkovitosti.
+  </p>
+
+  <p>
+    Korištenjem našeg servisa, potvrđujete da ste pročitali, razumjeli i prihvatili ove uvjete korištenja.
+  </p>
+
+  <p>
+    Za sva dodatna pitanja ili nedoumice, slobodno nas kontaktirajte.
+  </p>
+
+  <p>
+    Datum zadnje izmjene: 4.5.2024.
+  </p>
+  
+</FlexContainer>
+
+<Footer />
+
+<style>
+  p {
+    margin-top: 1rem;
+  }
+</style>
