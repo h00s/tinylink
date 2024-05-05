9f2048367f97ab73674ae5d49b21fc48d1fdc198
Krunoslav Husak <kruno@husak.me>
2024-05-05 23:03:12 +0200

    Footer opacity

--- frontend/src/lib/components/Footer.svelte
+++ frontend/src/lib/components/Footer.svelte
@@ -1,3 +1,3 @@
-<footer class="text-xs text-light-green">
+<footer class="text-xs text-light-green opacity-50">
   &copy; 2024. <a href="https://github.com/h00s">Krunoslav Husak</a> | <a href="https://github.com/h00s/tinylink">GitHub</a> | Izrađeno s 💚
 </footer>
