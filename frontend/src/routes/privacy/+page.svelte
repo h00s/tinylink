a3b867738817e2a7d6709d3e0a0a7d2fcfe9e273
Krunoslav Husak <kruno@husak.me>
2024-05-16 12:00:34 +0200

    Updated privacy and terms pages

--- frontend/src/routes/privacy/+page.svelte
+++ frontend/src/routes/privacy/+page.svelte
@@ -1,42 +1,35 @@
 <script>
-  import HeaderContainer from '$lib/components/HeaderContainer.svelte'
-  import Footer from '$lib/components/Footer.svelte'
+  import HeaderContainer from "$lib/components/HeaderContainer.svelte";
+  import Footer from "$lib/components/Footer.svelte";
 </script>
 
 <HeaderContainer>
-  <h2 class="pt-4 text-2xl font-extrabold text-center">
-    Pravila privatnosti
-  </h2>
+  <h2 class="pt-4 text-2xl font-extrabold text-center">Pravila privatnosti</h2>
 
   <p>
-    Ova Pravila privatnosti opisuju kako mi prikupljamo, koristimo i otkrivamo osobne podatke kada koristite našu uslugu i objašnjavaju vaša prava u vezi s tim podacima. Molimo vas pažljivo pročitajte ova pravila privatnosti prije nego što koristite našu uslugu.
+    Ova Pravila privatnosti opisuju kako prikupljam, koristim i otkrivam
+    osobne podatke kada koristite ovu uslugu i objašnjavaju vaša prava u vezi s
+    tim podacima. Molim vas, pažljivo pročitajte ova pravila privatnosti prije
+    nego što koristite ovu uslugu.
   </p>
   <p>
-    <b class="text-light-green">Prikupljanje podataka:</b> Pri korištenju naše usluge možemo prikupljati određene osobne podatke kao što su IP adrese, podaci o pregledniku, podaci o lokaciji i druge tehničke informacije koje nam pomažu poboljšati uslugu.
+    <b class="text-light-green">Prikupljanje podataka:</b> Pri korištenju ove usluge
+    ne prikupljaju se podaci kao što su IP adrese, podaci o pregledniku,
+    podaci o lokaciji i druge tehničke informacije. IP adresa se privremeno (do 24h)
+    pohranjuje u memoriji za potrebe ograničavanja prekomjernog korištenja usluge.
   </p>
   <p>
-    <b class="text-light-green">Upotreba podataka:</b> Osobne podatke koristimo isključivo radi pružanja i poboljšanja naše usluge. Ti podaci mogu se koristiti i za analizu trendova, administraciju web mjesta, praćenje kretanja korisnika i prikupljanje demografskih podataka.
+    <b class="text-light-green">Izmjene Pravila privatnosti:</b> Zadržavamo pravo
+    izmjene ovih Pravila privatnosti u bilo kojem trenutku. Bilo kakve promjene bit
+    će objavljene na ovoj stranici.
   </p>
   <p>
-    <b class="text-light-green">Otkrivanje podataka:</b> Nećemo prodavati, trgovati ili na bilo koji drugi način prenositi vaše osobne podatke trećim stranama bez vašeg pristanka, osim ako to nije potrebno radi pružanja usluge ili ako to zahtijeva zakon.
-  </p>
-  <p>
-    <b class="text-light-green">Sigurnost podataka:</b> Poduzimamo razumne mjere kako bismo zaštitili vaše osobne podatke od neovlaštenog pristupa, izmjene ili uništenja. Međutim, imajte na umu da nijedna metoda prijenosa preko interneta ili metoda elektroničke pohrane nije 100% sigurna i stoga ne možemo jamčiti apsolutnu sigurnost vaših podataka.
-  </p>
-  <p>
-    <b class="text-light-green">Dječja privatnost:</b> Naša usluga nije namijenjena osobama mlađim od 13 godina. Ne prikupljamo namjerno osobne podatke od djece mlađe od 13 godina. Ako saznamo da smo prikupili osobne podatke djeteta mlađeg od 13 godina, odmah ćemo poduzeti korake kako bismo te podatke izbrisali.
-  </p>
-  <p>  
-    <b class="text-light-green">Izmjene Pravila privatnosti:</b> Zadržavamo pravo izmjene ovih Pravila privatnosti u bilo kojem trenutku. Bilo kakve promjene bit će objavljene na ovoj stranici, stoga redovito provjeravajte ovu stranicu radi ažuriranja.
-  </p>
-  <p>
-    Vaša upotreba naše usluge podrazumijeva vaše prihvaćanje ovih Pravila privatnosti. Ako imate bilo kakvih pitanja ili nedoumica, slobodno nas kontaktirajte.
+    Vaša upotreba naše usluge podrazumijeva vaše prihvaćanje ovih Pravila
+    privatnosti. Ako imate bilo kakvih pitanja ili nedoumica, slobodno nas
+    kontaktirajte.
   </p>
 
-  <p>
-    Datum zadnje izmjene: 4.5.2024.
-  </p>
-  
+  <p>Datum zadnje izmjene: 16.5.2024.</p>
 </HeaderContainer>
 
 <Footer />
--- frontend/src/routes/terms/+page.svelte
+++ frontend/src/routes/terms/+page.svelte
@@ -1,51 +1,64 @@
 <script>
-  import HeaderContainer from '$lib/components/HeaderContainer.svelte'
-  import Footer from '$lib/components/Footer.svelte'
+  import HeaderContainer from "$lib/components/HeaderContainer.svelte";
+  import Footer from "$lib/components/Footer.svelte";
 </script>
 
 <HeaderContainer>
-  <h2 class="pt-4 text-2xl font-extrabold text-center">
-    Uvjeti korištenja
-  </h2>
-
-  <p>Korištenjem našeg usluga, pristajete na sljedeće uvjete korištenja. Molimo vas pažljivo pročitajte sljedeće informacije:</p>
+  <h2 class="pt-4 text-2xl font-extrabold text-center">Uvjeti korištenja</h2>
 
   <p>
-    <b class="text-light-green">Prihvaćanje uvjeta:</b> Pristupom i korištenjem naše usluge, pristajete na sve uvjete i odredbe ove stranice. Ako se ne slažete s bilo kojim dijelom ovih uvjeta, nemojte koristiti našu uslugu.
-  </p>
-  
-  <p>
-    <b class="text-light-green">Odgovornost korisnika:</b> Korištenje našeg servisa je isključivo na vašu vlastitu odgovornost. Vi ste odgovorni za bilo kakvu štetu ili gubitak koji proizađe iz korištenja naših usluga.
+    Korištenjem ove usluge, pristajete na sljedeće uvjete korištenja. Molim
+    vas pažljivo pročitajte sljedeće informacije:
   </p>
 
   <p>
-    <b class="text-light-green">Odgovornost vlasnika:</b> Vlasnik ovog servisa ne preuzima nikakvu odgovornost za sadržaj na koji se korisnici preusmjeravaju korištenjem našeg servisa. Ne garantiramo sigurnost, pouzdanost ili valjanost sadržaja na takvim stranicama.
+    <b class="text-light-green">Prihvaćanje uvjeta:</b> Pristupom i korištenjem ove
+    usluge, pristajete na sve uvjete i odredbe ove usluge. Ako se ne slažete s
+    bilo kojim dijelom ovih uvjeta, nemojte koristiti ovu uslugu.
   </p>
 
   <p>
-    <b class="text-light-green">Intelektualno vlasništvo:</b> Sadržaj koji pružamo, uključujući ali ne ograničavajući se na logotipe, tekst, grafike, fotografije, videozapise i ostale materijale, zaštićen je autorskim pravima i drugim zakonima o intelektualnom vlasništvu.
-  </p>  
-
-  <p>
-    <b class="text-light-green">Izmjene uvjeta:</b> Zadržavamo pravo izmjene ovih uvjeta korištenja u bilo kojem trenutku bez prethodne obavijesti. Vaša daljnja upotreba naše usluge nakon takvih promjena smatrat će se vašim prihvaćanjem izmjena.
+    <b class="text-light-green">Odgovornost korisnika:</b> Korištenje ove usluge
+    je isključivo na vašu vlastitu odgovornost. Vi ste odgovorni za bilo kakvu štetu
+    ili gubitak koji proizađe iz korištenja ove usluge.
   </p>
 
   <p>
-    <b class="text-light-green">Završne odredbe:</b> Ovi uvjeti korištenja predstavljaju cjelokupni dogovor između vas i nas u vezi s korištenjem naše usluge. Ako bilo koja odredba ovih uvjeta bude smatrana nevaljanom ili neizvršivom, ta odredba će se smatrati odvojivom od preostalih odredbi, koje će ostati u punoj snazi i učinkovitosti.
+    <b class="text-light-green">Odgovornost vlasnika:</b> Vlasnik ove usluge ne
+    preuzima nikakvu odgovornost za sadržaj na koji se korisnici preusmjeravaju korištenjem
+    ove usluge. Ne garantiram sigurnost, pouzdanost ili valjanost sadržaja na
+    takvim stranicama.
   </p>
 
   <p>
-    Korištenjem našeg servisa, potvrđujete da ste pročitali, razumjeli i prihvatili ove uvjete korištenja.
+    <b class="text-light-green">Intelektualno vlasništvo:</b> Sadržaj koji pružam,
+    uključujući ali ne ograničavajući se na logotipe, tekst, grafike, fotografije,
+    videozapise i ostale materijale apsolutno nije zaštićen autorskim pravima i drugim zakonima
+    o intelektualnom vlasništvu.
   </p>
 
   <p>
-    Za sva dodatna pitanja ili nedoumice, slobodno nas kontaktirajte.
+    <b class="text-light-green">Izmjene uvjeta:</b> Zadržavam pravo izmjene ovih
+    uvjeta korištenja u bilo kojem trenutku bez prethodne obavijesti. Vaša daljnja
+    upotreba ove usluge nakon takvih promjena smatrat će se vašim prihvaćanjem izmjena.
   </p>
 
   <p>
-    Datum zadnje izmjene: 4.5.2024.
+    <b class="text-light-green">Završne odredbe:</b> Ovi uvjeti korištenja predstavljaju
+    cjelokupni dogovor između vas i mene u vezi s korištenjem naše usluge. Ako bilo
+    koja odredba ovih uvjeta bude smatrana nevaljanom ili neizvršivom, ta odredba
+    će se smatrati odvojivom od preostalih odredbi, koje će ostati u punoj snazi
+    i učinkovitosti.
   </p>
-  
+
+  <p>
+    Korištenjem ove usluge, potvrđujete da ste pročitali, razumjeli i
+    prihvatili ove uvjete korištenja.
+  </p>
+
+  <p>Za sva dodatna pitanja ili nedoumice, slobodno me kontaktirajte.</p>
+
+  <p>Datum zadnje izmjene: 16.5.2024.</p>
 </HeaderContainer>
 
 <Footer />
